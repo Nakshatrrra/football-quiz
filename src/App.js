173a9e743cf3ae9b608d3c445965b5f5c2f24cd1
Nakshatra <nakshatratrivedi44@gmail.com>
2023-05-23 02:03:16 +0530

    change app name

--- src/App.js
+++ src/App.js
@@ -1,23 +1,229 @@
-import logo from './logo.svg';
-import './App.css';
+import "./app.css";
+import { useEffect, useMemo, useState } from "react";
+import Start from "./components/Start";
+import Timer from "./components/Timer";
+import Trivia from "./components/Trivia";
 
 function App() {
+  const [username, setUsername] = useState(null);
+  const [timeOut, setTimeOut] = useState(false);
+  const [questionNumber, setQuestionNumber] = useState(1);
+  const [earned, setEarned] = useState("$ 0");
+
+  const data = [
+    {
+      id: 1,
+      question: "Which team won won FIFA world cup 2022?",
+      answers: [
+        {
+          text: "Argetina",
+          correct: true,
+        },
+        {
+          text: "Brazil",
+          correct: true,
+        },
+        {
+          text: "Croatia",
+          correct: false,
+        },
+        {
+          text: "France",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 2,
+      question: "What is Nakshatra's favourite player",
+      answers: [
+        {
+          text: "Messi",
+          correct: true,
+        },
+        {
+          text: "Ronaldo",
+          correct: false,
+        },
+        {
+          text: "Neymar",
+          correct: false,
+        },
+        {
+          text: "Suarez",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "Who scored hattrick in world cup final and still lost?",
+      answers: [
+        {
+          text: "pique",
+          correct: false,
+        },
+        {
+          text: "ramos",
+          correct: false,
+        },
+        {
+          text: "messi",
+          correct: false,
+        },
+        {
+          text: "mbappe",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 4,
+      question: "What is Nakshatra's favourite player",
+      answers: [
+        {
+          text: "Messi",
+          correct: true,
+        },
+        {
+          text: "Ronaldo",
+          correct: false,
+        },
+        {
+          text: "Neymar",
+          correct: false,
+        },
+        {
+          text: "Suarez",
+          correct: false,
+        },
+      ],
+    }
+    
+    ,
+    {
+      id: 5,
+      question: "What is Nakshatra's favourite player",
+      answers: [
+        {
+          text: "Messi",
+          correct: true,
+        },
+        {
+          text: "Ronaldo",
+          correct: false,
+        },
+        {
+          text: "Neymar",
+          correct: false,
+        },
+        {
+          text: "Suarez",
+          correct: false,
+        },
+      ],
+    }
+    ,
+    {
+      id: 6,
+      question: "What is Nakshatra's favourite player",
+      answers: [
+        {
+          text: "Messi",
+          correct: true,
+        },
+        {
+          text: "Ronaldo",
+          correct: false,
+        },
+        {
+          text: "Neymar",
+          correct: false,
+        },
+        {
+          text: "Suarez",
+          correct: false,
+        },
+      ],
+    }
+    
+  ];
+
+  const moneyPyramid = useMemo(
+    () =>
+      [
+        { id: 1, amount: "$ 100" },
+        { id: 2, amount: "$ 200" },
+        { id: 3, amount: "$ 300" },
+        { id: 4, amount: "$ 500" },
+        { id: 5, amount: "$ 1.000" },
+        { id: 6, amount: "$ 2.000" },
+        { id: 7, amount: "$ 4.000" },
+        { id: 8, amount: "$ 8.000" },
+        { id: 9, amount: "$ 16.000" },
+        { id: 10, amount: "$ 32.000" },
+        { id: 11, amount: "$ 64.000" },
+        { id: 12, amount: "$ 125.000" },
+        { id: 13, amount: "$ 250.000" },
+        { id: 14, amount: "$ 500.000" },
+        { id: 15, amount: "$ 1.000.000" },
+      ].reverse(),
+    []
+  );
+
+  useEffect(() => {
+    questionNumber > 1 &&
+      setEarned(moneyPyramid.find((m) => m.id === questionNumber - 1).amount);
+  }, [questionNumber, moneyPyramid]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {!username ? (
+        <Start setUsername={setUsername} />
+      ) : (
+        <>
+          <div className="main">
+            {timeOut ? (
+              <h1 className="endText">You earned: {earned}</h1>
+            ) : (
+              <>
+                <div className="top">
+                  <div className="timer">
+                    <Timer
+                      setTimeOut={setTimeOut}
+                      questionNumber={questionNumber}
+                    />
+                  </div>
+                </div>
+                <div className="bottom">
+                  <Trivia
+                    data={data}
+                    questionNumber={questionNumber}
+                    setQuestionNumber={setQuestionNumber}
+                    setTimeOut={setTimeOut}
+                  />
+                </div>
+              </>
+            )}
+          </div>
+          <div className="pyramid">
+            <ul className="moneyList">
+              {moneyPyramid.map((m) => (
+                <li
+                  className={
+                    questionNumber === m.id
+                      ? "moneyListItem active"
+                      : "moneyListItem"
+                  }
+                >
+                  <span className="moneyListItemNumber">{m.id}</span>
+                  <span className="moneyListItemAmount">{m.amount}</span>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </>
+      )}
     </div>
   );
 }
